23a5994f7fe30438cb9b081ba203b8417558e746
Paulo Cesar Pereira de Andrade <pcpa@mandriva.com.br>
2008-01-30 17:18:31 -0200

    Don't call xf86XInputSetSendCoreEvents.
    Only call that function if GET_ABI_MAJOR(ABI_XINPUT_VERSION) == 0.

--- src/microtouch.c
+++ src/microtouch.c
@@ -157,7 +157,9 @@ static const char *reqSymbols[] = {
 	"xf86SetSerial",
 	"xf86SetStrOption",
 	"xf86XInputSetScreen",
+#if GET_ABI_MAJOR(ABI_XINPUT_VERSION) == 0
 	"xf86XInputSetSendCoreEvents",
+#endif
 	"xf86memset",
 	"xf86sscanf",
 	"xf86strcmp",
@@ -561,11 +563,13 @@ SwitchMode (ClientPtr client, DeviceIntPtr dev, int mode)
 		priv->reporting_mode = mode;
 		return (Success);
 	}
+#if GET_ABI_MAJOR(ABI_XINPUT_VERSION) == 0
 	else if ((mode == SendCoreEvents) || (mode == DontSendCoreEvents))
 	{
 		xf86XInputSetSendCoreEvents (local, (mode == SendCoreEvents));
 		return (Success);
 	}
+#endif
 	else
 		return (!Success);
 }
